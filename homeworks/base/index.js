12b3edf5f6c61cdfa36df38f1014b37a54789921
SergioKovalenko <sergey.koval.19@gmail.com>
2023-12-01 15:10:24 +0100

    base finished

--- homeworks/base/index.js
+++ homeworks/base/index.js
@@ -1,3 +1,34 @@
-// let name = prompt("Serhii");
+// coment in 1 line
 
-let name = prompt ('What is your name');
+/*coment in
+several lines */
+
+
+let username = prompt('What is your name?');
+
+const message = 'Hello, ' + username;
+
+console.log(message);
+
+
+let userAge = prompt('How old are u?');
+
+console.log(userAge);
+
+
+let gender = prompt('What is your gender?');
+
+console.log(gender);
+
+
+let civilianStatus = prompt('What is your civilian status?');
+
+console.log(civilianStatus);
+
+//
+
+let userYear = prompt('What is your birth year?');
+
+const age = 2023 - userYear;
+console.log('user age is ', age);
+
